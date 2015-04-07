f50844595a16fe981f3528abf7b65b9e692fd531
Abe Voelker <abe@abevoelker.com>
2015-04-07 14:24:06 -0500

    Begin homework 2

--- cis194/homework-2/src/HW2/LogAnalysis.hs
+++ cis194/homework-2/src/HW2/LogAnalysis.hs
@@ -10,10 +10,22 @@ module HW2.LogAnalysis
 import Provided.Log
 
 parseMessage :: String -> LogMessage
-parseMessage = undefined
+parseMessage x
+    | flag == "I" = LogMessage Info timestamp message
+    | flag == "W" = LogMessage Warning timestamp message
+    where flag      = (words x)!!0
+          timestamp = read ((words x)!!1) :: Int
+          message   = unwords (drop 2 (words x))
+parseMessage x
+    | flag == "E" = LogMessage (Error errcode) timestamp message
+    where flag      = (words x)!!0
+          errcode   = read ((words x)!!1) :: Int
+          timestamp = read ((words x)!!2) :: Int
+          message   = unwords (drop 3 (words x))
+parseMessage x = Unknown x
 
 parse :: String -> [LogMessage]
-parse = undefined
+parse x = map parseMessage (lines x)
 
 insert :: LogMessage -> MessageTree -> MessageTree
 insert = undefined
